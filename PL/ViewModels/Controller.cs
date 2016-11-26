272d119a9667734ed26939be3d9e771b1c05d7ca
reuvenp <reuvenp@checkpoint.com>
2016-11-26 23:01:34 +0200

    basic navigation

--- PL/ViewModels/Controller.cs
+++ PL/ViewModels/Controller.cs
@@ -9,6 +9,7 @@ namespace PL.ViewModels
 {
     public class Controller : BaseViewModel
     {
+        private static int _currentScreen = 1;
         public IBL bl_adapter;
         private BaseViewModel _viewModel;
         public BaseViewModel ViewModel
@@ -24,6 +25,32 @@ namespace PL.ViewModels
         {
             bl_adapter = BL_imp.Insatnce;
             ViewModel = new MainViewViewModel(bl_adapter);
+            Nav1To2 = new DelegateCommand(MainViewToBookDetails);
+        }
+
+        private DelegateCommand _nav1To2;
+        public DelegateCommand Nav1To2
+        {
+            get { return _nav1To2; }
+            set
+            {
+                _nav1To2 = value;
+                OnPropertyChanged("Nav1To2");
+            }
+        }
+
+        public void MainViewToBookDetails(object obj)
+        {
+            if (_currentScreen != 1)
+                return;
+            var mainView = ViewModel as MainViewViewModel;
+            if (mainView.SelectedBook == null)
+                return;
+            var book = mainView.SelectedBook;
+            ViewModel = new BookDetailsViewModel();
+            var bookDetails = ViewModel as BookDetailsViewModel;
+            bookDetails.SelectedBook = book;
+            _currentScreen = 2;
         }
     }
 }
