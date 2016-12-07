2163253a0e0525f86600811c54300220843a26e7
reuvenp <reuvenp@checkpoint.com>
2016-12-07 14:20:00 +0200

    cart start

--- PL/ViewModels/Controller.cs
+++ PL/ViewModels/Controller.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BL;
+using System.Collections.ObjectModel;
+using BE;
+using System.Windows;
 
 namespace PL.ViewModels
 {
@@ -26,6 +29,24 @@ namespace PL.ViewModels
             bl_adapter = BL_imp.Insatnce;
             ViewModel = new MainViewViewModel(bl_adapter);
             Nav1To2 = new DelegateCommand(MainViewToBookDetails);
+            Nav2Back = new DelegateCommand(BookDetailsBack);
+            Cart = new ObservableCollection<Book>();
+            AddToCartCMD = new DelegateCommand(AddToCart);
+        }
+
+        private DelegateCommand _addToCartCMD;
+        public DelegateCommand AddToCartCMD
+        {
+            get
+            {
+                return _addToCartCMD;
+            }
+
+            set
+            {
+                _addToCartCMD = value;
+                OnPropertyChanged("AddToCartCMD");
+            }
         }
 
         private DelegateCommand _nav1To2;
@@ -36,6 +57,21 @@ namespace PL.ViewModels
             {
                 _nav1To2 = value;
                 OnPropertyChanged("Nav1To2");
+            }
+        }
+
+        private DelegateCommand _nav2Back;
+        public DelegateCommand Nav2Back
+        {
+            get
+            {
+                return _nav2Back;
+            }
+
+            set
+            {
+                _nav2Back = value;
+                OnPropertyChanged("Nav2Back");
             }
         }
 
@@ -51,6 +87,39 @@ namespace PL.ViewModels
             var bookDetails = ViewModel as BookDetailsViewModel;
             bookDetails.SelectedBook = book;
             _currentScreen = 2;
+        }
+
+        public void BookDetailsBack(object obj)
+        {
+            if (_currentScreen != 2)
+                return;
+            ViewModel = new MainViewViewModel(bl_adapter);
+            _currentScreen = 1;
+        }
+
+        private ObservableCollection<Book> _cart;
+        public ObservableCollection<Book> Cart
+        {
+            get
+            {
+                return _cart;
+            }
+
+            set
+            {
+                _cart = value;
+                OnPropertyChanged("Cart");
+            }
+        }
+
+        public void AddToCart(object obj)
+        {
+            if (_currentScreen != 2)
+                return;
+            var bookDetailsView = ViewModel as BookDetailsViewModel;
+            var book = bookDetailsView.SelectedBook;
+            Cart.Add(book);
+            MessageBox.Show("Book " + book.Title + " Was Added!");
         }
     }
 }
